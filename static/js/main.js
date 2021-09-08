f44b9ad7bf17382b3d4c14c7acced2148d85d036
Mat CHAN <klchan.358@gmail.com>
2021-09-09 00:05:45 +0800

    fix: that ugly navigation bar
    - now using js to make it looks nicer

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+// TODO: remove this javascript to have a better control on paginator
+window.addEventListener("load", function () {
+    const paginator_el = $(".PageNavigation a");
+    if (paginator_el.length <= 1) {
+        paginator_el.toggleClass("col-md-12");
+    }
+})
