fc7d00f1fa29fc7b5f7f228afd8f987bb3be49a3
Mat CHAN <klchan.358@gmail.com>
2021-09-19 02:21:30 +0800

    update: css name to meet the standard

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 // TODO: remove this javascript to have a better control on paginator
 window.addEventListener("load", function () {
-    const paginator_el = $(".PageNavigation a");
+    const paginator_el = $(".page-navigation a");
     if (paginator_el.length <= 1) {
         paginator_el.toggleClass("col-md-12");
     }
